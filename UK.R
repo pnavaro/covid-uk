69408e7bd9e7c415cdf9b0a51f17262cede3a2f5
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-05-05 16:31:51 +0200

    Add some dependencies

--- UK.R
+++ UK.R
@@ -12,6 +12,7 @@ covid_uk_path = here()
 
 # covidm options
 cm_path = paste0(covid_uk_path, "/covidm/");
+print(cm_path)
 if (grepl(Sys.info()["user"], pattern = "^adamkuchars(ki)?$")) { cm_path = "~/Documents/GitHub/covid-uk/covidm/" }
 source(paste0(cm_path, "/R/covidm.R"))
 
--- install.R
+++ install.R
@@ -1,2 +1,2 @@
-packages <- c("here", "stringi", "data.table", "qs", "HDInterval", "RcppGSL")
+packages <- c("here", "lubridate", "nloptr", "ggplot2", "stringr", "stringi", "data.table", "qs", "HDInterval", "RcppGSL")
 install.packages(setdiff(packages, rownames(installed.packages())))  
