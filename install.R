43ad29c880c2db38d90f0d868198131480a50375
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-05-05 17:38:23 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,17 @@
-packages <- c("here", "lubridate", "nloptr", "ggplot2", "stringr", "stringi", "data.table", "qs", "HDInterval", "RcppGSL")
-install.packages(setdiff(packages, rownames(installed.packages())))  
+packages <- c(
+"here", 
+"lubridate", 
+"nloptr", 
+"ggplot2", 
+"stringr", 
+"stringi", 
+"data.table", 
+"qs", 
+"HDInterval", 
+"Rcpp", 
+"RcppGSL", 
+"countrycode", 
+"wpp2019", 
+"socialmixr", 
+"readxl")
+install.packages(packages)  
